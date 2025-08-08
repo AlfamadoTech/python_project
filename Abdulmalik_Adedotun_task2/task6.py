5228c478c93238cf96215fc735bb9140f906d6c7
Abdulmalik <61978007+alfamado@users.noreply.github.com>
2025-08-08 23:36:58 +0300

    Add files via upload

--- Abdulmalik_Adedotun_task2/task6.py
+++ Abdulmalik_Adedotun_task2/task6.py
@@ -1,3 +1,5 @@
 customer_full_name = input("What is your name ")
 units_consumed = int(input("How much unit did you consumed in KWh "))
-cost_per_unit = float(input("Unit consumed "))
+cost_per_unit = float(input("Cost per unit is "))
+total_bill = units_consumed * cost_per_unit
+print(f"Customer {customer_full_name}\nused {units_consumed} unit\nand the cost of each unit in KWh is {cost_per_unit}/n Total = {total_bill}")
