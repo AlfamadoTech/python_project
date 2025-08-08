99b6cf87f8501136f6a7e43fe0ed811842b2fe88
Abdulmalik <61978007+alfamado@users.noreply.github.com>
2025-08-09 00:00:56 +0300

    Update task4.py

--- Abdulmalik_Adedotun_task2/task4.py
+++ Abdulmalik_Adedotun_task2/task4.py
@@ -1,3 +1,4 @@
+# 4. Nigeria Dish Popularity
 nigeria_dish = input("Name one Nigeria dish: ")
 state_dish = input("What state is that dish popular in: ")
 print("favourite Nigeria dish and the State it's popular in")
