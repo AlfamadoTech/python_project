91854132d4ea8c36fb298fd2696d60bb916c4e43
Abdulmalik <61978007+alfamado@users.noreply.github.com>
2025-08-08 23:59:14 +0300

    Update task4.py

--- Abdulmalik_Adedotun_task2/task4.py
+++ Abdulmalik_Adedotun_task2/task4.py
@@ -1,4 +1,4 @@
-nigeria_dish = input("Name one Nigeria dish ")
-state_dish = input("What state is that dish popular in ")
-
-print
+nigeria_dish = input("Name one Nigeria dish: ")
+state_dish = input("What state is that dish popular in: ")
+print("favourite Nigeria dish and the State it's popular in")
+print(f"one of Nigeria favourite dish is\t{nigeria_dish}\nstate popular in\t\t\t{state_dish}")
