a6bb081e020c34ff361e9b7b123be73fb67e0037
Abdulmalik <61978007+alfamado@users.noreply.github.com>
2025-08-09 00:05:05 +0300

    Update task8.py

--- Abdulmalik_Adedotun_task2/task8.py
+++ Abdulmalik_Adedotun_task2/task8.py
@@ -1,3 +1,4 @@
+# Distance covered and ticket price
 distance_in_km = float(input("How many distance in kilometer did you covered "))
 fare_per_km = float(input("Price of transport per kilometer is "))
 transport_fare = distance_in_km * fare_per_km
